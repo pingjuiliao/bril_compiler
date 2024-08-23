e5a161051d2f321a868345c72eb08f462333efb5
pingjui <pingjuiliao@gmail.com>
2024-08-23 15:58:36 -0700

    optimize json dump

--- bril_compiler/program.py
+++ bril_compiler/program.py
@@ -52,6 +52,34 @@ class Function(object):
     def add_argument(self, name, arg_type):
         self.arguments.append((name, arg_type))
 
+    def dump_json(self):
+        function_json = {}
+        # function name
+        function_json["name"] = self.get_identifier()
+
+        # function arguemnts:
+        function_json['args'] = []
+        for arg_name, arg_type in self.arguments:
+            function_json['args'].append({
+                'name': arg_name,
+                'type': arg_type
+            })
+
+        # instrs
+        function_json['instrs'] = []
+        for basic_block in self.get_basic_blocks():
+            label_instr = basic_block.get_label()
+            if label_instr is not None:
+                label_json = label_instr.dump_json()
+                function_json['instrs'].append(label_json)
+
+            for instr in basic_block.get_instructions():
+                instr_json = instr.dump_json()
+                function_json['instrs'].append(instr_json)
+
+        return function_json
+
+
 class Module(object):
     def __init__(self):
         self._functions = []
@@ -66,31 +94,7 @@ class Module(object):
         module_json = {}
         module_json["functions"] = []
         for function in self._functions:
-            function_json = {}
-            # function name
-            function_json["name"] = function.get_identifier()
-
-            # function arguments
-            function_json['args'] = []
-            for argument in function.arguments:
-                function_json['args'].append({
-                    'name': argument[0],
-                    'type': argument[1],
-                })
-
-            # function instructions
-            function_json["instrs"] = []
-            for basic_block in function.get_basic_blocks():
-                # any basic block may contain one label instruction
-                label_instr = basic_block.get_label()
-                if label_instr is not None:
-                    label_json = label_instr.dump_json()
-                    function_json["instrs"].append(label_json)
-
-                # append the rest of instruction
-                for instruction in basic_block.get_instructions():
-                    instr_json = instruction.dump_json()
-                    function_json["instrs"].append(instr_json)
+            function_json = function.dump_json()
             module_json["functions"].append(function_json)
         # json.dump(module_json, sys.stdout)
         return module_json
